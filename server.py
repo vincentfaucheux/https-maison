47ebfc7d31c950cd58d511f0c4616d0f61adf1f1
Vincent Faucheux <vincent.faucheux@gmail.com>
2025-11-15 17:11:42 +0100

    init, result  fenetre modifier une fois

--- server.py
+++ server.py
@@ -4,13 +4,55 @@ from typing import Any
 app = Flask(__name__)
 
 RADIATEUR_CONFIGS : dict[int, dict[str,Any]] = {
-    1: { "nom": "Salle", "x": "70", "y": "200", "width": "30", "height": "10"},
-    2: { "nom": "Salon", "x": "325", "y": "50", "width": "30", "height": "10"},
-    3: { "nom": "Salle cuisine", "x": "200", "y": "390", "width": "30", "height": "10"},
-    4: { "nom": "Cuisine", "x": "240", "y": "410", "width": "10", "height": "30"},
-    5: { "nom": "Entree", "x": "315", "y": "500", "width": "10", "height": "30"},
-    6: { "nom": "Salle de bain", "x": "540", "y": "325", "width": "10", "height": "30"},
-    7: { "nom": "Chambre", "x": "500", "y": "50", "width": "30", "height": "10"},
+    1: { "nom": "Radiateur Salle", 
+         "groupe": "Pièces a vivre", 
+         "x": "70", 
+         "y": "200", 
+         "width": "30", 
+         "height": "10"
+        },
+    2: { "nom": "Radiateur Salon", 
+         "groupe": "Pièces a vivre",
+         "x": "325", 
+         "y": "50", 
+         "width": "30", 
+         "height": "10"
+        },
+    3: { "nom": "Radiateur Salle mur cuisine", 
+         "groupe": "Pièces a vivre",
+         "x": "200", 
+         "y": "390", 
+         "width": "30", 
+         "height": "10"
+        },
+    4: { "nom": "Radiateur Cuisine", 
+         "groupe": "Pièces a vivre",
+         "x": "240", 
+         "y": "410", 
+         "width": "10", 
+         "height": "30"
+        },
+    5: { "nom": "Radiateur Entree", 
+         "groupe": "Pièces a vivre",
+         "x": "315", 
+         "y": "500", 
+         "width": "10", 
+         "height": "30"
+        },
+    6: { "nom": "Radiateur Salle de bain", 
+         "groupe": "Salle de bain",
+         "x": "540", 
+         "y": "325", 
+         "width": "10", 
+         "height": "30"
+        },
+    7: { "nom": "Radiateur Chambre", 
+         "groupe": "Chambre",
+         "x": "500", 
+         "y": "50", 
+         "width": "30", 
+         "height": "10"
+        },
 }
 
 RADIATEUR_STATES = {
@@ -21,6 +63,16 @@ RADIATEUR_STATES = {
     5: 1,
     6: 1,
     7: 1,
+}
+
+RADIATEUR_TIME_FORCE = {
+    1: { "heure": 0, "minute": 0 }, 
+    2: { "heure": 0, "minute": 0 }, 
+    3: { "heure": 0, "minute": 0 }, 
+    4: { "heure": 0, "minute": 0 }, 
+    5: { "heure": 0, "minute": 0 }, 
+    6: { "heure": 0, "minute": 0 }, 
+    7: { "heure": 0, "minute": 0 }, 
 }
 
 
@@ -39,6 +91,8 @@ def rezdechausse():
         radiateur_data = radiateur_data.copy()
         radiateur_data['id'] = id
         radiateur_data['state'] = RADIATEUR_STATES[id]
+        radiateur_data['timeforce'] = RADIATEUR_TIME_FORCE[id]
+        # Example schedule
         radiateur_data['schedule'] = [
             ("07:00", "conf"),
             ("23:00", "eco")
@@ -52,10 +106,15 @@ def rezdechausse():
 def set_new_state(radiateur_id: str):
     # get the parameter
     new_state = request.json.get("state")
+    force_heure = request.json.get("forceheure")
+    force_minute = request.json.get("forceminute")
 
     # save the new state to the RadiateurState list
     RADIATEUR_STATES[int(radiateur_id)] = new_state
-
+    RADIATEUR_TIME_FORCE[int(radiateur_id)] = { 
+        "heure": force_heure,
+        "minute": force_minute
+    }
     return {"status": "success", "state": new_state}, 200
 
 
